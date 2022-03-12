e0b59c6d247e3d0d2b66706f1933cc455168dfa0
DogwishX <zefiru@protonmail.com>
2022-03-12 04:52:19 +0000

    Add DOM Manipulation

---
+++ dom-manipulation/index.js
@@ -0,0 +1,43 @@
+// DOM Containers
+const buttonEl = document.querySelector(".codename__button");
+const inputEl = document.querySelector(".codename__input");
+let inputValue = inputEl.value;
+
+// Event Listeners
+inputEl.addEventListener("input", handleInputUpdate);
+buttonEl.addEventListener("click", startValidation);
+
+function handleInputUpdate({ target }) {
+  inputValue = target.value;
+  startValidation();
+}
+
+// Validation
+const checkLength = (str) => str.length > 5;
+const hasLowerUppercase = (str) => /(?=.*[a-z])(?=.*[A-Z]).*/g.test(str);
+const hasNumbers = (str) => (str.match(/[0-9]/g) || []).length >= 2;
+let isCodenameValid = false;
+
+function startValidation() {
+  if (validateCodename()) {
+    applyStyling(true);
+  } else {
+    applyStyling(false);
+  }
+}
+
+function validateCodename() {
+  const validations = [
+    checkLength(inputValue),
+    hasLowerUppercase(inputValue),
+    hasNumbers(inputValue),
+  ];
+
+  return validations.every((validation) => validation === true);
+}
+
+function applyStyling(boolean) {
+  console.log(boolean);
+  if (boolean) inputEl.classList.add("valid");
+  else inputEl.classList.remove("valid");
+}

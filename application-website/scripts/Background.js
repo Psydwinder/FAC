793905f5bae2fd374dd9d6c92199a76155a33160
DogwishX <zefiru@protonmail.com>
2022-04-11 11:20:33 +0100

    Add comments to file as well as refactor cleanupSpeheres()

--- application-website/scripts/Background.js
+++ application-website/scripts/Background.js
@@ -5,42 +5,22 @@ const backgroundDrawSpeed = 10;
 const backgroundExecution = setInterval(drawBackground, backgroundDrawSpeed);
 const sphereCreation = setInterval(createSphere, 500);
 const sphereCleanup = setInterval(cleanupSpheres, 10000);
-
 let spheres = [];
-class Sphere {
-  constructor() {
-    this.x = 0;
-    this.y = Math.random() * window.innerHeight;
-    this.yMovement = Math.random() / 5;
-    this.xMovement = Math.random() / 3;
-    // Movement along the Y axis has been set to a slower speed than the X axis, prioritizing left to right movement over up or down;
-    this.upOrDown = Math.random() < 0.5 ? -this.yMovement : +this.yMovement;
-    this.size = Math.random() * 1.5 + 1;
-  }
-
-  draw() {
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-    ctx.fillStyle = "#4d3c82";
-    ctx.fill();
-    ctx.closePath();
-  }
-
-  move() {
-    this.x += this.xMovement;
-    this.y += this.upOrDown;
-  }
-}
 
 function drawBackground() {
-  resizeCanvas();
+  resizeCanvas(); // Redraws the whole canvas, avoiding trails
   drawSpheres();
+}
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight < 450 ? 450 : window.innerHeight;
 }
 
 function drawSpheres() {
   // For loop used instead of array method to improve performance
   for (
-    let currentSphere = 0, spheresLength = spheres.length; // cache length for performance
+    let currentSphere = 0, spheresLength = spheres.length; // cache array length for performance
     currentSphere < spheresLength;
     currentSphere++
   ) {
@@ -55,16 +35,38 @@ function createSphere() {
 
 function cleanupSpheres() {
   // Removes spheres that are currently out of bounds to improve performance
-  spheres = spheres.filter((sphere) => {
-    const { x, y } = sphere;
-    if (x <= window.innerWidth && y <= window.innerHeight && y >= 0)
-      return sphere;
-  });
+  spheres = spheres.filter(
+    (sphere) =>
+      sphere.x <= window.innerWidth &&
+      sphere.y <= window.innerHeight &&
+      sphere.y >= 0 &&
+      sphere
+  );
 }
 
-function resizeCanvas() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight < 450 ? 450 : window.innerHeight;
+class Sphere {
+  constructor() {
+    this.x = 0;
+    this.y = Math.random() * window.innerHeight;
+    // Movement along the Y axis has been set to a slower speed than the X axis, prioritizing left to right movement over up or down;
+    this.yMovement = Math.random() / 5;
+    this.xMovement = Math.random() / 3;
+    this.upOrDown = Math.random() < 0.5 ? -this.yMovement : +this.yMovement;
+    this.size = Math.random() * 1.5 + 1; // Minimum size of sphere set to be at least one
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2); // (x, y, radius, startAngle, endAngle) endAngle set to 2pi radians (360deg)
+    ctx.fillStyle = "#4d3c82";
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  move() {
+    this.x += this.xMovement;
+    this.y += this.upOrDown;
+  }
 }
 
 export { resizeCanvas };

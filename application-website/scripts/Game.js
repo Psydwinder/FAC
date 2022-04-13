2b29ab0dccec74dba5eb653e5f3d8c896e9247f4
DogwishX <zefiru@protonmail.com>
2022-04-13 22:11:21 +0100

    Add game background image

--- application-website/scripts/Game.js
+++ application-website/scripts/Game.js
@@ -1,14 +1,42 @@
 const canvas = document.querySelector("#game");
 const ctx = canvas.getContext("2d");
 
-function drawCharacter() {
-    
+canvas.addEventListener("click", startGame);
+
+let hasGameStarted = false,
+  oneViewWidth,
+  oneViewHeight;
+
+function startGame() {
+  hasGameStarted = true;
+  drawGame();
+}
+
+function drawGame() {
+  drawGameBackground();
+}
+
+function drawCharacter() {}
+
+function drawGameBackground() {
+  const backgroundImg = new Image();
+  backgroundImg.src = "./media/game-background.png";
+  backgroundImg.onload = () => ctx.drawImage(backgroundImg, 0, 0);
 }
 
 function resizeGame() {
-  const oneViewWidth = window.innerWidth / 100;
-  canvas.height = oneViewWidth * 50;
-  canvas.width = oneViewWidth * 50;
+  oneViewWidth = window.innerWidth / 100;
+  oneViewHeight = window.innerHeight / 100;
+  canvas.height = oneViewHeight * 50;
+  canvas.width = oneViewWidth * 70;
 }
+
+class Character {
+  constructor(spriteSrc) {
+    drawCharacter(spriteSrc);
+  }
+}
+
+// Background Image NightForest by saukgp - https://saurabhkgp.itch.io/pixel-art-forest-background-simple-seamless-parallax-ready-for-2d-platformer-s
 
 export { resizeGame };

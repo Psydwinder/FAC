8f2b0275c0ad8f9d9d3723cebfea74084fc7c09c
Dogwish <zefiru@protonmail.com>
2022-04-21 03:20:41 +0100

    General refactoring and cleanup

--- application-website/scripts/Game.js
+++ application-website/scripts/Game.js
@@ -1,5 +1,4 @@
 // TODO
-// Add SFX
 // Refactor
 // Improve hitboxes
 // Add sprite animations
@@ -8,6 +7,7 @@
 
 import { firestore, db } from "./firebase.js";
 
+// DOM Containers
 const canvas = document.querySelector("#game__canvas");
 const ctx = canvas.getContext("2d");
 const gameForm = document.querySelector(".game__form");
@@ -19,61 +19,95 @@ canvas.height = 360;
 canvas.width = 620;
 const floorPositionY = canvas.height - 145;
 
+// Game init
 gameBtn.addEventListener("click", init);
 
-let hasGameStarted = false;
+// Game settings
 let isMusicPlaying = false;
+let hasGameStarted = false;
 let score = 0;
 let gameVelocity;
-let createVerticalEnemies;
-let createHorizontalEnemies;
-let updateDifficultyInterval;
+let setVerticalEnemiesInterval;
+let setHorizontalEnemiesInterval;
+let setDifficultyInterval;
 let verticalInterval = 1000;
 let horizontalInterval = 2000;
 
 function init() {
-  gameForm.style = "display: none";
-  if (hasGameStarted) return;
-  if (!isMusicPlaying) {
-    playSound("music.mp3");
-    isMusicPlaying = true;
-    setTimeout(() => (isMusicPlaying = false), 100000);
-  }
   hasGameStarted = true;
+  toggleGameForm();
+  loopBackgroundMusic();
   setIntervals();
-  window.addEventListener("keydown", handleKeyDown);
-  window.addEventListener("keyup", handleKeyUp);
-  ctx.fillStyle = "white";
-  function drawGame() {
-    if (hasGameStarted) {
-      window.requestAnimationFrame(drawGame);
-      drawScore();
-      // background.draw();
-      drawGameBackground();
-      updateObjects();
-      movePlayer();
-      clearEnemies();
-      clearProjectiles();
-    }
-  }
+  addEventListeners();
   drawGame();
 }
 
-function resetCanvas() {
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+function toggleGameForm() {
+  gameForm.classList.toggle("game__form--active");
+}
+
+function loopBackgroundMusic() {
+  if (!isMusicPlaying) {
+    const trackDuration = 100000;
+    playSound("music.mp3");
+    isMusicPlaying = true;
+    setTimeout(() => (isMusicPlaying = false), trackDuration);
+  }
+}
+
+function playSound(soundName, volume = 1) {
+  const sound = new Audio(`./media/game-assets/sfx/${soundName}`);
+  sound.volume = volume;
+  sound.play();
 }
 
 function setIntervals() {
-  createVerticalEnemies = setInterval(createVerticalEnemy, 1000);
-  createHorizontalEnemies = setInterval(createHorizontalEnemy, 2000);
-  updateDifficultyInterval = setInterval(updateDifficulty, 1000);
+  setVerticalEnemiesInterval = setInterval(createVerticalEnemy, 1000);
+  setHorizontalEnemiesInterval = setInterval(createHorizontalEnemy, 2000);
+  setDifficultyInterval = setInterval(updateDifficulty, 1000);
+}
+
+function addEventListeners() {
+  window.addEventListener("keydown", handleKeyDown);
+  window.addEventListener("keyup", handleKeyUp);
+}
+
+function drawGame() {
+  if (hasGameStarted) {
+    window.requestAnimationFrame(drawGame);
+    drawScore();
+    drawGameBackground();
+    updateObjects();
+    movePlayer();
+    clearEnemies();
+    clearProjectiles();
+  }
+}
+
+function drawScore() {
+  const drawScoreBg = () => {
+    ctx.fillStyle = "black";
+    ctx.fillRect(canvas.width - 120, 0, 120, 70);
+  };
+  const drawScoreText = () => {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "white";
+    ctx.fillText(`Score:  ${score}`, canvas.width - 120, 30);
+    ctx.fillText(
+      `Hi-score:  ${localStorage.hiScore || score}`,
+      canvas.width - 120,
+      50
+    );
+  };
+
+  drawScoreBg();
+  drawScoreText();
 }
 
 function clearIntervals() {
-  clearInterval(createHorizontalEnemies);
-  clearInterval(createVerticalEnemies);
-  clearInterval(updateDifficultyInterval);
+  clearInterval(setHorizontalEnemiesInterval);
+  clearInterval(setVerticalEnemiesInterval);
+  clearInterval(setDifficultyInterval);
 }
 
 function updateObjects() {
@@ -88,36 +122,23 @@ function updateDifficulty() {
     clearIntervals();
     horizontalInterval -= 50;
     verticalInterval -= 50;
-    createHorizontalEnemies = setInterval(
+    setHorizontalEnemiesInterval = setInterval(
       createHorizontalEnemy,
       horizontalInterval
     );
-    createVerticalEnemies = setInterval(createVerticalEnemy, verticalInterval);
+    setVerticalEnemiesInterval = setInterval(
+      createVerticalEnemy,
+      verticalInterval
+    );
   }
 }
 
-function playSound(soundName, volume = 1) {
-  const sound = new Audio(`./media/game-assets/sfx/${soundName}`);
-  sound.volume = volume;
-  sound.play();
-  console.log("sound played");
-}
-
 function drawGameBackground() {
-  // Background Image NightForest by saukgp - https://saurabhkgp.itch.io/pixel-art-forest-background-simple-seamless-parallax-ready-for-2d-platformer-s
+  //
   const backgroundImg = new Image();
   backgroundImg.src = "./media/game-assets/game-background.png";
   backgroundImg.onload = () =>
     ctx.drawImage(backgroundImg, 0, 0, canvas.width, canvas.height);
-}
-
-function drawScore() {
-  ctx.fillStyle = "black";
-  ctx.fillRect(canvas.width - 120, 0, 120, 70);
-  ctx.font = "16px Arial";
-  ctx.fillStyle = "white";
-  ctx.fillText(`Score:  ${score}`, canvas.width - 120, 30);
-  ctx.fillText(`Hi-score:  ${localStorage.hiScore}`, canvas.width - 120, 50);
 }
 
 class Sprite {
@@ -148,25 +169,6 @@ class Sprite {
       );
   }
 }
-
-const background = new Sprite({
-  position: {
-    x: 0,
-    y: 0,
-  },
-  dimensions: {
-    height: canvas.height,
-    width: canvas.width,
-  },
-  frame: {
-    x: 0,
-    y: 0,
-    width: canvas.width,
-    height: canvas.height,
-  },
-  imageSrc: "./media/game-assets/game-background.png",
-});
-
 class Character extends Sprite {
   constructor(obj) {
     const { position, velocity, dimensions, gravity = 0.7 } = obj;
@@ -190,6 +192,72 @@ class Character extends Sprite {
   }
 }
 
+let enemies = [];
+class Enemy extends Character {
+  constructor(obj) {
+    super(obj);
+    this.leftOrRight = Math.random() <= 0.5 ? -1 : 1;
+    this.hasCollided = false;
+    this.hp = 1;
+  }
+
+  update() {
+    this.draw();
+    this.detectProjectileCollision();
+    this.detectPlayerCollision();
+    this.x += this.velocity.x * this.leftOrRight;
+    this.y += this.velocity.y;
+  }
+
+  detectProjectileCollision() {
+    projectiles.forEach((projectile) => {
+      if (
+        projectile.x >= this.x &&
+        projectile.x <= this.x + this.width &&
+        projectile.y >= this.y &&
+        projectile.y <= this.y + this.height
+      ) {
+        this.hp -= 1;
+        projectile.hasCollided = true;
+        score += 50;
+        if (this.hp === 0) this.hasCollided = true;
+        playSound("hit.mp3", 0.2);
+      }
+    });
+  }
+
+  detectPlayerCollision() {
+    if (
+      player.x + player.width > this.x &&
+      player.x < this.x + this.width &&
+      player.y + player.height > this.y &&
+      player.y < this.y + this.height
+    )
+      gameOver();
+  }
+}
+
+let projectiles = [];
+class Projectile {
+  constructor() {
+    this.x = player.x + player.width / 2;
+    this.y = player.y;
+    this.height = 7;
+    this.width = 7;
+    this.hasCollided = false;
+  }
+
+  draw() {
+    ctx.fillStyle = "#cf2527";
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+  update() {
+    this.draw();
+    this.y += -5;
+  }
+}
+
 // Player initialization
 const player = new Character({
   velocity: {
@@ -201,7 +269,7 @@ const player = new Character({
     height: 100,
   },
   position: {
-    x: canvas.width / 2,
+    x: canvas.width / 2 - 52, // 52 character width
     y: floorPositionY,
   },
 
@@ -251,59 +319,17 @@ function movePlayer() {
   player.velocity.x = 0;
 }
 
-// Enemies
-let enemies = [];
-if (hasGameStarted) {
+function shoot() {
+  projectiles.push(new Projectile());
+  playSound("shoot.mp3", 0.2);
 }
 
+// Enemies
 function clearEnemies() {
   enemies = enemies.filter(
     (enemy) =>
       enemy.x <= canvas.width && enemy.y <= canvas.height && !enemy.hasCollided
   );
-}
-class Enemy extends Character {
-  constructor(obj) {
-    super(obj);
-    this.leftOrRight = Math.random() <= 0.5 ? -1 : 1;
-    this.hasCollided = false;
-    this.hp = 1;
-  }
-
-  update() {
-    this.draw();
-    this.detectProjectileCollision();
-    this.detectPlayerCollision();
-    this.x += this.velocity.x * this.leftOrRight;
-    this.y += this.velocity.y;
-  }
-
-  detectProjectileCollision() {
-    projectiles.forEach((projectile) => {
-      if (
-        projectile.x >= this.x &&
-        projectile.x <= this.x + this.width &&
-        projectile.y >= this.y &&
-        projectile.y <= this.y + this.height
-      ) {
-        this.hp -= 1;
-        projectile.hasCollided = true;
-        score += 50;
-        if (this.hp === 0) this.hasCollided = true;
-        playSound("hit.mp3", 0.2);
-      }
-    });
-  }
-
-  detectPlayerCollision() {
-    if (
-      player.x + player.width > this.x &&
-      player.x < this.x + this.width &&
-      player.y + player.height > this.y &&
-      player.y < this.y + this.height
-    )
-      gameOver();
-  }
 }
 
 function createVerticalEnemy() {
@@ -363,7 +389,6 @@ function createHorizontalEnemy() {
 }
 
 // Projectiles
-let projectiles = [];
 function clearProjectiles() {
   projectiles = projectiles.filter(
     (projectile) =>
@@ -373,35 +398,8 @@ function clearProjectiles() {
   );
 }
 
-class Projectile {
-  constructor() {
-    this.x = player.x + player.width / 2;
-    this.y = player.y;
-    this.height = 7;
-    this.width = 7;
-    this.hasCollided = false;
-  }
-
-  draw() {
-    ctx.fillStyle = "#cf2527";
-    ctx.fillRect(this.x, this.y, this.width, this.height);
-  }
-
-  update() {
-    this.draw();
-    this.y += -5;
-  }
-}
-
-function shoot() {
-  projectiles.push(new Projectile());
-  playSound("shoot.mp3", 0.2);
-}
-
+// End of game
 function gameOver() {
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "white";
-  ctx.fillText("Game Over!", canvas.width / 2, canvas.height / 2);
   playSound("game-over.mp3", 1);
   hasGameStarted = false;
   enemies = [];
@@ -410,9 +408,10 @@ function gameOver() {
   gameVelocity = 0;
   horizontalInterval = 2000;
   verticalInterval = 1000;
-  addScoreToDb();
-  gameForm.style.display = "block";
+  toggleGameForm();
   clearIntervals();
+  drawScore();
+  addScoreToDb();
 }
 
 async function addScoreToDb() {
@@ -423,14 +422,9 @@ async function addScoreToDb() {
   await firestore.updateDoc(hiscoreRef, {
     users: tempArr,
   });
-  displayRanking();
+  displayTop10();
 
   score = 0;
-}
-
-async function displayRanking() {
-  const { sortedHiscoreData } = await fetchData();
-  displayTop10(sortedHiscoreData);
 }
 
 async function fetchData() {
@@ -443,8 +437,8 @@ async function fetchData() {
   return { hiscoreRef, hiscoreSnap, hiscoreData, sortedHiscoreData };
 }
 
-function displayTop10(sortedHiscoreData) {
-  const top10Div = document.querySelector(".ranking__top10");
+async function displayTop10() {
+  const { sortedHiscoreData } = await fetchData();
   const top10 = sortedHiscoreData.slice(0, 10);
   const rankingList = document.querySelector(".ranking__top10--list");
   const itemTemplate = document.querySelector(".ranking-template");
@@ -453,16 +447,18 @@ function displayTop10(sortedHiscoreData) {
   top10.forEach(({ name, score }, index) => {
     const newItemClone = itemTemplate.content.cloneNode(true);
     const newItem = newItemClone.querySelector(".ranking__top10--item");
+    const newItemPersonContainer = newItemClone.querySelector(
+      ".ranking__person-container"
+    );
     const newItemName = newItemClone.querySelector(".ranking__name");
     const newItemScore = newItemClone.querySelector(".ranking__score");
 
-    newItemName.innerText = `${index + 1} - ${
-      name === "" ? "Anonymous" : name
-    }`;
+    newItemName.innerText = `${name === "" ? "Anonymous" : name}`;
     newItemScore.innerText = score;
-    newItem.append(newItemName, newItemScore);
+    newItemPersonContainer.append(newItemName, newItemScore);
+    newItem.append(newItemPersonContainer);
     rankingList.append(newItem);
   });
 }
 
-displayRanking();
+displayTop10();

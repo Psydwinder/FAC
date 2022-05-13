c3d5426b485ed9c9c2b9871cd3c809242c155297
Dogwish <zefiru@protonmail.com>
2022-05-13 15:52:26 +0100

    Add Colour constructor class

--- colour-palette/scripts/Main.js
+++ colour-palette/scripts/Main.js
@@ -16,3 +16,35 @@ function generateRandomColor() {
 function randomNumber(possibilities) {
   return Math.floor(Math.random() * possibilities);
 }
+
+class Colour {
+  constructor({ colour, colourName, isLocked = false }) {
+    this.colour = colour;
+    this.colourName = colourName;
+    this.isLocked = isLocked;
+
+    // DOM related
+    this.container = document.createElement("div");
+    this.container.classList.add("colours__item");
+  }
+
+  render() {
+    const coloursContainer = document.querySelector(".colours__container");
+    this.container.innerHTML = `
+      <div class='colours__representation'>${this.colour}</div>
+    `;
+    coloursContainer.append(this.container);
+  }
+}
+
+const colours = {
+  primary: new Colour({ colour: "primary" }),
+  secondary: new Colour({ colour: "secondary" }),
+  tertiary: new Colour({ colour: "tertiary" }),
+};
+
+function renderAllColours() {
+  for (let colour in colours) {
+    colours[colour].render();
+  }
+}

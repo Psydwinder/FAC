e0797c5fbcf4ce4ee0126cbc61daf7cf3b41734d
Dogwish <zefiru@protonmail.com>
2022-05-29 05:38:31 +0100

    Add functionality to save palette to `localStorage`

--- colour-palette/scripts/Main.js
+++ colour-palette/scripts/Main.js
@@ -1,6 +1,7 @@
 import "./Random.js";
 import { actionAllColours } from "./utils.js";
 import { coloursArr } from "./Colours.js";
+import "./Save.js";
 import "./Download.js";
 import "./RandomRelatedAI.js";
 
--- colour-palette/scripts/Save.js
+++ colour-palette/scripts/Save.js
@@ -1,15 +1,30 @@
+import { coloursArr } from "./Colours.js";
+import { createNotification } from "./utils.js";
+
+// Modal display
+const modal = document.querySelector(".modal");
 const saveBtn = document.querySelector(".nav__save");
-// const modal = document.querySelector(".modal");
-// const modalSaveBtn = document.querySelector(".modal__save");
+
+saveBtn.addEventListener("click", toggleModal);
+
+function toggleModal() {
+  modal.classList.toggle("modal--active");
+}
+
+// Save palette name
+const modalSaveBtn = document.querySelector(".modal__button");
 const paletteNameInput = document.querySelector("#palette-name");
 
-saveBtn.addEventListener("click", displayModal);
-// modalSaveBtn.addEventListener("click", saveToLocalStorage);
-
-function displayModal() {}
+modalSaveBtn.addEventListener("click", saveToLocalStorage);
 
 function saveToLocalStorage() {
   const savedPalettes = JSON.parse(localStorage.palettes || "{}");
   savedPalettes[paletteNameInput.value] = coloursArr;
   localStorage.setItem("palettes", JSON.stringify(savedPalettes));
+  toggleModal();
+  createNotification("Palette saved succesfully!");
 }
+
+// Close modal
+const modalCloseBtn = document.querySelector(".modal__close");
+modalCloseBtn.addEventListener("click", toggleModal);
